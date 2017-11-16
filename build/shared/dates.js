f9819e4a5f437014f7dfd29a76ccc975d1837b1e
Olivier Oudry <olivier@a2r.io>
2017-11-16 14:56:12 +0100

    add build folder for direct install from github

---
+++ build/shared/dates.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getISOLocalDate = exports.getISOLocalMonth = exports.getEnd = exports.getBegin = exports.getDaysInMonth = exports.getDay = exports.getMonthIndex = exports.getMonth = exports.getYear = undefined;
+
+var _dates = require('react-calendar/build/shared/dates');
+
+exports.getYear = _dates.getYear;
+exports.getMonth = _dates.getMonth;
+exports.getMonthIndex = _dates.getMonthIndex;
+exports.getDay = _dates.getDay;
+exports.getDaysInMonth = _dates.getDaysInMonth;
+exports.getBegin = _dates.getBegin;
+exports.getEnd = _dates.getEnd;
+exports.getISOLocalMonth = _dates.getISOLocalMonth;
+exports.getISOLocalDate = _dates.getISOLocalDate;
